dcc6b3ce644a29faa4fbb0712b27d6019d6310f5
Deleplace <deleplace2015@gmail.com>
2020-05-19 16:13:30 +0200

    False sharing: add some writes, every 20 reads.

--- falsesharing/falsesharing_test.go
+++ falsesharing/falsesharing_test.go
@@ -5,10 +5,13 @@ import (
 	"testing"
 )
 
+// Test performance of memory access: lots of reads, and a
+// few writes to discard some cache lines.
+
 const M = 20000000
 
 // Sink makes sure results are not optimized away by the compiler
-var Sink int
+var Sink int64
 
 var SinkMu sync.Mutex
 
@@ -16,46 +19,52 @@ const expectedSum = (M * (M + 1)) / 2
 
 // How fast is it to access a[0], a[1], ..., a[M-1] ?
 func BenchmarkAccessForward(b *testing.B) {
-	a := make([]int, M)
+	a := make([]int64, M)
 	for i := range a {
-		a[i] = 1 + i
+		a[i] = int64(1 + i)
 	}
 
 	b.ResetTimer()
-	var sum int
+	var sum int64
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < M; j++ {
 			sum += a[j]
+			if j%20 == 19 {
+				a[j], a[j-1] = a[j-1], a[j]
+			}
 		}
 	}
 	b.StopTimer()
 	SinkMu.Lock()
 	Sink = sum
 	SinkMu.Unlock()
-	if sum/b.N != expectedSum {
+	if sum/int64(b.N) != expectedSum {
 		b.Errorf("Wrong sum %d != %d", sum, expectedSum)
 	}
 }
 
 // How fast is it to access a[M-1], a[M-2], ..., a[0] ?
 func BenchmarkAccessBackward(b *testing.B) {
-	a := make([]int, M)
+	a := make([]int64, M)
 	for i := range a {
-		a[i] = 1 + i
+		a[i] = int64(1 + i)
 	}
 
 	b.ResetTimer()
-	var sum int
+	var sum int64
 	for i := 0; i < b.N; i++ {
 		for j := M - 1; j >= 0; j-- {
 			sum += a[j]
+			if j%20 == 18 {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
 		}
 	}
 	b.StopTimer()
 	SinkMu.Lock()
 	Sink = sum
 	SinkMu.Unlock()
-	if sum/b.N != expectedSum {
+	if sum/int64(b.N) != expectedSum {
 		b.Errorf("Wrong sum %d != %d", sum, expectedSum)
 	}
 }
@@ -64,13 +73,13 @@ func BenchmarkAccessBackward(b *testing.B) {
 // a[0], ..., a[M/2-1]
 // a[M/2], ..., a[M-1]
 func BenchmarkAccessConcurrentForwardForward(b *testing.B) {
-	a := make([]int, M)
+	a := make([]int64, M)
 	for i := range a {
-		a[i] = 1 + i
+		a[i] = int64(1 + i)
 	}
 
 	b.ResetTimer()
-	var sum1, sum2 int
+	var sum1, sum2 int64
 	for i := 0; i < b.N; i++ {
 		var wg sync.WaitGroup
 		wg.Add(2)
@@ -78,12 +87,18 @@ func BenchmarkAccessConcurrentForwardForward(b *testing.B) {
 		go func() {
 			for j := 0; j < middle; j++ {
 				sum1 += a[j]
+				if j%20 == 19 {
+					a[j], a[j-1] = a[j-1], a[j]
+				}
 			}
 			wg.Done()
 		}()
 		go func() {
 			for j := middle; j < M; j++ {
 				sum2 += a[j]
+				if j%20 == 19 {
+					a[j], a[j-1] = a[j-1], a[j]
+				}
 			}
 			wg.Done()
 		}()
@@ -95,7 +110,7 @@ func BenchmarkAccessConcurrentForwardForward(b *testing.B) {
 	SinkMu.Lock()
 	Sink = sum
 	SinkMu.Unlock()
-	if sum/b.N != expectedSum {
+	if sum/int64(b.N) != expectedSum {
 		b.Errorf("Wrong sum %d != %d", sum, expectedSum)
 	}
 }
@@ -104,13 +119,13 @@ func BenchmarkAccessConcurrentForwardForward(b *testing.B) {
 // a[0], ..., a[M/2-1]
 // a[M-1], ..., a[M/2]
 func BenchmarkAccessConcurrentForwardBackward(b *testing.B) {
-	a := make([]int, M)
+	a := make([]int64, M)
 	for i := range a {
-		a[i] = 1 + i
+		a[i] = int64(1 + i)
 	}
 
 	b.ResetTimer()
-	var sum1, sum2 int
+	var sum1, sum2 int64
 	for i := 0; i < b.N; i++ {
 		var wg sync.WaitGroup
 		wg.Add(2)
@@ -118,12 +133,18 @@ func BenchmarkAccessConcurrentForwardBackward(b *testing.B) {
 		go func() {
 			for j := 0; j < middle; j++ {
 				sum1 += a[j]
+				if j%20 == 19 {
+					a[j], a[j-1] = a[j-1], a[j]
+				}
 			}
 			wg.Done()
 		}()
 		go func() {
 			for j := M - 1; j >= middle; j-- {
 				sum2 += a[j]
+				if j%20 == 18 {
+					a[j], a[j+1] = a[j+1], a[j]
+				}
 			}
 			wg.Done()
 		}()
@@ -135,7 +156,7 @@ func BenchmarkAccessConcurrentForwardBackward(b *testing.B) {
 	SinkMu.Lock()
 	Sink = sum
 	SinkMu.Unlock()
-	if sum/b.N != expectedSum {
+	if sum/int64(b.N) != expectedSum {
 		b.Errorf("Wrong sum %d != %d", sum, expectedSum)
 	}
 }
@@ -144,13 +165,13 @@ func BenchmarkAccessConcurrentForwardBackward(b *testing.B) {
 // a[0], a[2], ..., a[M/2-2]
 // a[1], a[3], ..., a[M/2-1]
 func BenchmarkAccessConcurrentInterleaved(b *testing.B) {
-	a := make([]int, M)
+	a := make([]int64, M)
 	for i := range a {
-		a[i] = 1 + i
+		a[i] = int64(1 + i)
 	}
 
 	b.ResetTimer()
-	var sum1, sum2 int
+	var sum1, sum2 int64
 	for i := 0; i < b.N; i++ {
 		var wg sync.WaitGroup
 		wg.Add(2)
@@ -158,24 +179,29 @@ func BenchmarkAccessConcurrentInterleaved(b *testing.B) {
 		go func() {
 			for j := 0; j < M; j += 2 {
 				sum1 += a[j]
+				if j%40 == 18 {
+					a[j], a[j-2] = a[j-2], a[j]
+				}
 			}
 			wg.Done()
 		}()
 		go func() {
 			for j := 1; j < M; j += 2 {
 				sum2 += a[j]
+				if j%40 == 19 {
+					a[j], a[j-2] = a[j-2], a[j]
+				}
 			}
 			wg.Done()
 		}()
 		wg.Wait()
-
 	}
 	b.StopTimer()
 	sum := sum1 + sum2
 	SinkMu.Lock()
 	Sink = sum
 	SinkMu.Unlock()
-	if sum/b.N != expectedSum {
+	if sum/int64(b.N) != expectedSum {
 		b.Errorf("Wrong sum %d != %d", sum, expectedSum)
 	}
 }
