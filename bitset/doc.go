3eaf8cee16a15f2239ce130b42a74e247fbf32e9
Deleplace <deleplace2015@gmail.com>
2020-08-14 17:48:55 +0200

    Bitset: this code are belong to Google.

---
+++ bitset/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2020 Google LLC
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+Package bitset is sample code illustrating possible implementations
+of a Bitset in Go, with benchmarks.
+*/
+package bitset
