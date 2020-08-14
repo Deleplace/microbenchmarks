9a7b4c16f82c2459931e35b072023cae7f243ca2
Deleplace <deleplace2015@gmail.com>
2020-08-14 17:50:31 +0200

    Bitset map: delete false entries, to free up space.

--- bitset/bitset_map.go
+++ bitset/bitset_map.go
@@ -7,7 +7,13 @@ func (b BitsetMap) GetBit(i int) bool {
 }
 
 func (b BitsetMap) SetBit(i int, value bool) {
-	b[i] = value
+	if value {
+		b[i] = value
+	} else {
+		// deleting a value may free up space,
+		// while setting it to false will probably not.
+		delete(b, i)
+	}
 }
 
 func (b BitsetMap) Len() int {
